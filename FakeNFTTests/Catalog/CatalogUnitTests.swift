4994f7b3a0ef10a2c3decf82fe6a6e1357230c26
Makovey Vladislav <vladislav.makovey@raiffeisen.ru>
2024-04-05 15:36:35 +0300

    added unit-tests for catalog

---
+++ FakeNFTTests/Catalog/CatalogUnitTests.swift
@@ -0,0 +1,90 @@
+//
+//  CatalogUnitTests.swift
+//  FakeNFTTests
+//
+//  Created by MAKOVEY Vladislav on 03.04.2024.
+//
+
+@testable import FakeNFT
+import XCTest
+
+final class CatalogUnitTests: XCTestCase {
+    func test_presenterLoadCatalogItems_whenViewDidLoad() {
+        // arrange
+        let presenter = CatalogPresenter(
+            router: CatalogRouterSpy(),
+            service: CatalogItemServiceStub(state: .success),
+            sortStorage: SortStorageStub(type: .none)
+        )
+
+        let view = CatalogViewSpy()
+        presenter.view = view
+
+        // act
+        presenter.viewDidLoad()
+
+        // assert
+        XCTAssertTrue(view.invokedShowLoader)
+        XCTAssertTrue(view.invokedUpdateCatalogItems)
+        XCTAssertTrue(view.invokedDismissLoader)
+    }
+
+    func test_presenterShowsError_whenItemsLoadsWithError() {
+        // arrange
+        let presenter = CatalogPresenter(
+            router: CatalogRouterSpy(),
+            service: CatalogItemServiceStub(state: .failure),
+            sortStorage: SortStorageStub(type: .none)
+        )
+
+        let view = CatalogViewSpy()
+        presenter.view = view
+
+        // act
+        presenter.viewDidLoad()
+
+        // assert
+        XCTAssertTrue(view.invokedShowLoader)
+        XCTAssertTrue(view.invokedShowError)
+        XCTAssertTrue(view.invokedDismissLoader)
+    }
+
+    func test_presenterLoadItems_whenPullToRefreshDragged() {
+        // arrange
+        let presenter = CatalogPresenter(
+            router: CatalogRouterSpy(),
+            service: CatalogItemServiceStub(state: .success),
+            sortStorage: SortStorageStub(type: .none)
+        )
+
+        let view = CatalogViewSpy()
+        presenter.view = view
+
+        // act
+        presenter.pullToRefreshDragged()
+
+        // assert
+        XCTAssertTrue(view.invokedShowLoader)
+        XCTAssertTrue(view.invokedUpdateCatalogItems)
+        XCTAssertTrue(view.invokedDismissLoader)
+    }
+
+    func test_presenterShowsAlert_whenSortButtonTapped() {
+        // arrange
+        let presenter = CatalogPresenter(
+            router: CatalogRouterSpy(),
+            service: CatalogItemServiceStub(state: .success),
+            sortStorage: SortStorageStub(type: .none)
+        )
+
+        let view = CatalogViewSpy()
+        presenter.view = view
+
+        // act
+        presenter.sortButtonTapped()
+
+        // assert
+        XCTAssertTrue(view.invokedShowSortingAlert)
+    }
+
+}
